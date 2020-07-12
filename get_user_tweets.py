ed1150001945279e9dc465cf708cebb0066feabe
luiz felipe campos <61940871+camposfelipeluiz@users.noreply.github.com>
2020-07-12 17:00:05 -0300

    Update get_user_tweets.py

--- get_user_tweets.py
+++ get_user_tweets.py
@@ -1,7 +1,7 @@
 import tweepy
 import pandas as pd
 
-def get_user_tweets(user, items=100):
+def get_user_tweets(user1, user2,  user1_items=100, user2_items=100):
     
     consumer_key = ''
     consumer_secret = ''
@@ -10,18 +10,35 @@ def get_user_tweets(user, items=100):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    list_= []
-    public_tweets = tweepy.Cursor(api.user_timeline, tweet_mode='extended', screen_name=user).items(items)
-    for tweet in public_tweets:
+    
+    list1= []
+    public_tweets1 = tweepy.Cursor(api.user_timeline, tweet_mode='extended', screen_name=user1).items(items1)
+    for tweet in public_tweets1:
         if 'RT @' not in tweet.full_text:
-            dict_ = {'screen_name': tweet.user.screen_name, 'tweet': tweet.full_text, 'created_at': tweet.created_at, 'favorite_count': tweet.favorite_count,  'retweet_count': tweet.retweet_count}
-            list_.append(dict_)
-    df = pd.DataFrame(list_)
-    df['date'] = df['created_at'].apply(lambda x:x.date())
-    df['time'] = df['created_at'].apply(lambda x:x.time())
-    df['created_at'] = df['created_at'].apply(lambda x:x.strftime('%Y-%m-%d %H:%M'))
-    df['rts/likes'] = df['retweet_count']/df['favorite_count']
-    df['rts/likes'] = df['rts/likes'].round(2)
+            dict1 = {'screen_name': tweet.user.screen_name, 'tweet': tweet.full_text, 'created_at': tweet.created_at, 'favorite_count': tweet.favorite_count,  'retweet_count': tweet.retweet_count}
+            list1.append(dict1)
+    df1 = pd.DataFrame(list1)
+    df1['date'] = df1['created_at'].apply(lambda x:x.date())
+    df1['time'] = df1['created_at'].apply(lambda x:x.time())
+    df1['created_at'] = df1['created_at'].apply(lambda x:x.strftime('%Y-%m-%d %H:%M'))
+    df1['rts/likes'] = df1['retweet_count']/df1['favorite_count']
+    df1['rts/likes'] = df1['rts/likes'].round(2)
+    
+    list2 = []
+    public_tweets2 = tweepy.Cursor(api.user_timeline, tweet_mode='extended', screen_name=user2).items(items2)
+    for tweet in public_tweets2:
+        if 'RT @' not in tweet.full_text:
+            dict2 = {'screen_name': tweet.user.screen_name, 'tweet': tweet.full_text, 'created_at': tweet.created_at, 'favorite_count': tweet.favorite_count,  'retweet_count': tweet.retweet_count}
+            list2.append(dict2)
+    df2 = pd.DataFrame(list2)
+    df2['date'] = df2['created_at'].apply(lambda x:x.date())
+    df2['time'] = df2['created_at'].apply(lambda x:x.time())
+    df2['created_at'] = df2['created_at'].apply(lambda x:x.strftime('%Y-%m-%d %H:%M'))
+    df2['rts/likes'] = df2['retweet_count']/df2['favorite_count']
+    df2['rts/likes'] = df2['rts/likes'].round(2)
+    
+    df = pd.concat([df1, df2])
     
     return df
-    
+
+df = get_user_tweets()
