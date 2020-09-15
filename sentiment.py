b1737b433b4fbf15800693f51643daad05cfc971
luiz felipe campos <61940871+camposfelipeluiz@users.noreply.github.com>
2020-09-15 14:30:35 -0300

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -17,10 +17,13 @@ def sentiment(query, since, until, items):
     public_tweets = tweepy.Cursor(api.search, tweet_mode='extended', q=query, 
                               lang='en', since=since, until=until).items(items)
     for tweet in public_tweets:
+        
+        if 'RT @' not in tweet.full_text:
         analysis = TextBlob(tweet.full_text)
         polarity = analysis.sentiment.polarity
         subjectivity = analysis.sentiment.subjectivity
         retweet_count = tweet.retweet_count
+        
         if (retweet_count > 0) and (polarity != 0 or subjectivity != 0):
             dict_ = {'screen_name': tweet.user.screen_name, 
                  'tweet': tweet.full_text, 'date': tweet.created_at, 'polarity': polarity, 'subjectivity': subjectivity, 'favorite_count': tweet.favorite_count,
