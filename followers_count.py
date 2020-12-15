1a5a012174d9bbe85e5b069e2b07110c4e832d2f
luiz felipe campos <61940871+camposfelipeluiz@users.noreply.github.com>
2020-12-15 13:54:03 +0000

    Update followers_count.py

--- followers_count.py
+++ followers_count.py
@@ -1,24 +1,24 @@
 import tweepy
 import pandas as pd
 
-def followers_count(*users):
+def get_followers(*users):
 
     consumer_key = ''
     consumer_secret = ''
     access_token = ''
     access_token_secret = ''
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    screen_names = []
+    usuarios = []
     for user in users:
         user = api.get_user(str(user))._json
-        screen_names.append(user)
-        
-        k = len(screen_names)
+        usuarios.append(user)
+        k = len(usuarios)
         for k in range(0,k):
-            name = usuarios[k]['screen_name']
+            nome = usuarios[k]['screen_name']
             followers = usuarios[k]['followers_count']
-        
+            
         print(f'{nome}: {followers}')
